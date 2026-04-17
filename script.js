21d46f362cbffc031a0f00fbb8ff6f24ab6093b3
Vinícius Santos da Silva <vinicius.santos.135@ufrn.edu.br>
2026-04-17 15:23:55 -0300

    ajustando camera novamente

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
-//import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js';
 //testar no github page
 //configurando cena
 const scene = new THREE.Scene();
