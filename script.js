257e51e0dede8bd0e9b70fc6bc84695b851ffda9
Vinícius Santos da Silva <vinicius.santos.135@ufrn.edu.br>
2026-04-17 14:41:13 -0300

    biblioteca orbitcontroles

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
-//import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js';//ainda não utilizei
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js';//ainda não utilizei
 //testar no github page
 //configurando cena
 const scene = new THREE.Scene();
