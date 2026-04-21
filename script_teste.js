2429618ea5477e730e11207a3889b2bec6560a87
Vinícius Santos da Silva <vinicius.santos.135@ufrn.edu.br>
2026-04-21 09:22:57 -0300

    simplificando teste

--- script_teste.js
+++ script_teste.js
@@ -18,68 +18,39 @@ camera.position.z = 10;
 const carro = new THREE.Group();
 const corCarro = 0xe30f00; // cinza quase brancod3d3d3
 //corpo do carro
-function CriarCorpo(){
+var xBase = 2, yBase = 0.85, zBase = 3;
+var xRelevo = 2, yRelevo = 0.8, zRelevo = 1.7;
+function CriarCorpo(largura,altura,comprimento,posicao_y, cor){
   const body = new THREE.Mesh(
-    new THREE.BoxGeometry(2, 0.85, 3),//largura,altura,comprimento
-    new THREE.MeshBasicMaterial({color: corCarro}) //cor e textura do material (material escolhido = básico, cor sólida)
+    new THREE.BoxGeometry(largura, altura, comprimento),//largura,altura,comprimento
+    new THREE.MeshBasicMaterial({color: cor}) //cor e textura do material (material escolhido = básico, cor sólida)
   );
+  body.position.y = posicao_y; //move o objeto na cena em 3d
   return body;
 }
-carro.add(CriarCorpo());
-
-function CriaSombra(base, altura,x,y,z){
-  const sombra = new THREE.Mesh(
-    new THREE.PlaneGeometry(base,altura),
-    new THREE.MeshBasicMaterial({color: 0x4f221f})//antes 808080
-  );
-  sombra.position.set(x,y,z);
-  return sombra;
-}
-const x1=0, y1=0.42,z1=0.69, y2=0.003,z2=1.55; //posições das sombras
-carro.add(CriaSombra(2,1, x1,y1,z1));
-carro.add(CriaSombra(2,0.82, x1,y2,z2));
-
-//relevo do carro
-function criarRelevo(){
-  const relevo = new THREE.Mesh(
-    new THREE.BoxGeometry(2, 0.8, 1.3),//largura,altura,comprimento 
-    new THREE.MeshBasicMaterial({color: corCarro})//cor antiga:030bfc
-  );
-  relevo.position.y = 0.5; //move o objeto na cena em 3d
-  return relevo;
-}
-carro.add(criarRelevo());
+const base = CriarCorpo(xBase, yBase, zBase, 0, corCarro);
+const relevo = CriarCorpo(xRelevo, yRelevo, zRelevo, 0.5, corCarro);
+carro.add(base);
+carro.add(relevo);
 
 //rodas
-function criarRodas(x,z, a,b,cor){
+function criarRodas(posicao_x,posicao_z, raio_e_altura,pontas, cor){
   const roda = new THREE.Mesh(
-    new THREE.CylinderGeometry(a, a, a, b),//raio,altura,seg.radiais
+    new THREE.CylinderGeometry(raio_e_altura, raio_e_altura, raio_e_altura, pontas),//a = raio das bases do cilindro(2 primeiras letras),altura, b = seg.radiais(quantidade de pontas que forma)
     new THREE.MeshBasicMaterial({ color: cor })
   );
-  roda.rotation.z = Math.PI/2;// rotaciona o objeto
-  roda.position.set(x, -0.6,z); 
+  roda.rotation.z = Math.PI/2;// rotaciona o objeto em 90 graus no eixo z
+  roda.position.set(posicao_x, -0.6,posicao_z); 
   return roda;
 }
-const x = 0.9, xAro = 0.97, a=0.4, aAro= 0.3, b=8, b2=6, corRoda = 0x000000, corAro= 0x808080;
-carro.add(criarRodas(x,x,a,b,corRoda));
-carro.add(criarRodas(-x,x,a,b,corRoda));
-carro.add(criarRodas(x,-x,a,b,corRoda));
-carro.add(criarRodas(-x,-x,a,b,corRoda));
-//efeito do aro da roda
-carro.add(criarRodas(xAro,x,aAro,b2,corAro));
-carro.add(criarRodas(-xAro,x,aAro,b2,corAro));
-carro.add(criarRodas(xAro,-x,aAro,b2,corAro));
-carro.add(criarRodas(-xAro,-x,aAro,b2,corAro));
+const posicao_x = 0.9, posicao_z = 0.9, raio_e_altura=0.4, Quant_pontas=8, corRoda = 0x535454;
+carro.add(criarRodas(posicao_x,posicao_z, raio_e_altura,Quant_pontas,corRoda));
+carro.add(criarRodas(-posicao_x,posicao_z, raio_e_altura,Quant_pontas,corRoda));
+carro.add(criarRodas(posicao_x,-posicao_z, raio_e_altura,Quant_pontas,corRoda));
+carro.add(criarRodas(-posicao_x,-posicao_z, raio_e_altura,Quant_pontas,corRoda));
 //---------------------|carro pronto|-----------------------\\
 
 scene.add(carro);//adiciona carro a cena
-
-const fundo = new THREE.Mesh(
-  new THREE.PlaneGeometry(1000, 1000),//largura,altura
-  new THREE.MeshBasicMaterial({color: 0xcfcec2}) //cor e textura do material (material escolhido = básico, cor sólida)
-);
-fundo.position.z = -10;//move o objeto na cena em 3d
-scene.add(fundo);
 
 function animacao(){
   requestAnimationFrame(animacao);
